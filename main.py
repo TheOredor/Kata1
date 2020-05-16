708376ba4157c6f544897ae46996482d7f91db64
TheOredor <jrm_96_@hotmail.es>
2020-05-16 10:46:26 +0200

    Primera subida

---
+++ main.py
@@ -0,0 +1,27 @@
+'''
+    Esto es un comentarioo en bloque
+    que comenta varias lineas desde el principio 
+    hasta el final
+'''
+#Esto es un comentario en linea
+
+variableString1 = 'esto es una variable de string'
+variableString2 = "esto es una variable de string"
+
+variableNumerica = 25
+variableNumerica = variableNumerica + 5
+
+variableBoolean = True
+variableBoolean = False
+
+print('imprime por pantalla')
+dato = input('Introduce un dato: ')
+print(dato)
+
+if (variableNumerica == 30):
+    prueba = 'variable de prueba'
+    print('variable numerica es: ')
+    print(variableNumerica)
+    print('Y la variable de prueba contiene: ' + prueba)
+else:
+    print('variable numerica no es 30')
